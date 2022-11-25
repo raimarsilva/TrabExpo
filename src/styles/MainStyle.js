ee94a7cbad9b066da2991b97666d5d87a17c1d97
Hugo Oliveira <hugotholiveira@hotmail.com>
2022-11-25 02:11:57 -0300

    fix: Readicionado MainStyle

---
+++ src/styles/MainStyle.js
@@ -0,0 +1,54 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#eee',
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    titleText: {
+      fontSize:24,
+      textAlign: 'center',
+      marginVertical:5,
+      marginHorizontal:5,
+      marginBottom:50
+    },
+    plainText:{
+      fontSize:20
+    },
+    textInput: {
+      borderWidth:1,
+      placeholderTextColor:'grey',
+      borderRadius:4,
+      fontSize: 24,
+      marginVertical:10,
+      marginHorizontal:10,
+      width:'90%',
+      backgroundColor:'#fff',
+      height:50,
+      paddingHorizontal:5
+    },
+    button:{
+      backgroundColor:'#1351B4',
+      borderRadius:25,
+      width:'90%',
+      maxWidth: 400,
+      marginVertical:10,
+      marginHorizontal:10,
+      marginBottom:50,
+      height:50
+    },
+    buttonText:{
+      color:'#fff',
+      fontSize:20,
+      textAlignVertical:'center',
+      textAlign:'center',
+      height:50
+    },
+    flatlistStyle:{
+      
+    }
+  });
+
+  export default styles;
