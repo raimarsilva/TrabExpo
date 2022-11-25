0c9584f8639728a81d4c824f3e70ed3ca670d22d
Luan Santos <luan@tootz.com.br>
2022-11-25 13:39:33 -0300

    refactor: login

--- src/views/LoginScreen.js
+++ src/views/LoginScreen.js
@@ -1,82 +1,83 @@
-import React, { useContext} from 'react';
+import React, { useContext, useState } from "react";
 import { View, StyleSheet } from "react-native";
-import { Appbar, Button, TextInput } from 'react-native-paper';
+import { Appbar, Button, TextInput } from "react-native-paper";
 
-import {AuthContext} from '../context/AuthContext';
+import { AuthContext } from "../context/AuthContext";
 
 export default function LoginScreen({ navigation }) {
-  const {onSubmitHandler} = useContext(AuthContext);
+  const { login } = useContext(AuthContext);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
-  function onSubmit(user, password){
-    const res = onSubmitHandler(user, password);
-    if (res != 200){
-      alert("usuário ou senha inválido")
-    }
-  }
-  
+  const handleSubmitLoginForm = (user, password) => {
+    setIsLoading(true);
+    login(user, password)
+      .catch((error) => console.log(error.message))
+      .finally(() => setIsLoading(false));
+  };
+
   return (
     <View style={{ flex: 1 }}>
-      <Appbar.Header
-        mode="center-aligned">
+      <Appbar.Header mode="center-aligned">
         <Appbar.Content title="Autenticação" />
       </Appbar.Header>
       <View style={{ flex: 1 }}>
         <View style={styles.container}>
           <TextInput
-            //value={username}
+            value={username}
             mode="outlined"
             label="Usuário"
             placeholder="Seu nome de usuário"
             style={styles.textInput}
-          //onChangeText={onChangeUsernameHandler}
+            onChangeText={setUsername}
           />
           <TextInput
             secureTextEntry
-            //value={password}
+            value={password}
             mode="outlined"
-            label='Senha'
-            placeholder='Senha de 6 ou mais dígitos'
+            label="Senha"
+            placeholder="Senha de 6 ou mais dígitos"
             right={<TextInput.Icon icon="eye" />}
             style={styles.textInput}
-          //onChangeText={onChangePasswordHandler}
+            onChangeText={setPassword}
           />
           <Button
             mode="contained"
-            onPress={onSubmit}
-            style={styles.button}>
+            onPress={() => handleSubmitLoginForm(username, password)}
+            style={styles.button}
+            disabled={isLoading}
+          >
             Login
           </Button>
-          <Button
-            mode="text"
-          //onPress={onSubmitHandler}
-            style={styles.button}>
+          <Button mode="text" style={styles.button}>
             Esqueci minha senha
           </Button>
           <Button
             mode="outlined"
-            onPress={() => navigation.navigate('Register')}
-            style={styles.button}>
-            Cadastrar  
+            onPress={() => navigation.navigate("Register")}
+            style={styles.button}
+          >
+            Cadastrar
           </Button>
         </View>
       </View>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
-    backgroundColor: '#fff',
-    justifyContent: 'center',
+    flexDirection: "column",
+    backgroundColor: "#fff",
+    justifyContent: "center",
     paddingHorizontal: 20,
     paddingBottom: 64,
   },
-  textInput: {
-  },
+  textInput: {},
   button: {
     marginTop: 8,
     marginHorizontal: 16,
-  }
+  },
 });
