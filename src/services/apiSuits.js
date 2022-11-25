1ab523ae860af7d3e701ffe386513f28df17c83c
Hugo Oliveira <hugotholiveira@hotmail.com>
2022-11-25 14:38:16 -0300

    Feat: adicionado estilização da tela de Registro

--- src/services/apiSuits.js
+++ src/services/apiSuits.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJyYWltYXIiLCJleHAiOjE2NjkzNDAyNzZ9.YxgE3vElB_JyHe2UoEEsisRRZSKpINUYGd4o31kDSGc";
 
 const apiSuits = axios.create({
-    baseURL: 'http://192.168.1.7:8080/api',
+    baseURL: 'http://192.168.1.15:8080/api',
     headers: {'Authorization': 'Bearer '+token}
 });
 
