2af52c8725f899d7f4e19b74d6380c272824be72
jldms <lucastr140@gmail.com>
2022-11-25 04:30:28 -0300

    estilizando tela advogados

--- src/views/HomeScreen.js
+++ src/views/HomeScreen.js
@@ -1,17 +1,28 @@
-import { StatusBar } from "expo-status-bar";
-import { Button, Text, View } from "react-native";
-import styles from "../styles/MainStyle";
+import { Card, Title, Paragraph, IconButton, Button } from 'react-native-paper';
+import { View, StyleSheet } from "react-native";
 
-export default function HomeScreen({navigation}){
-    return(
-        <View style={styles.container}>
-            <StatusBar style="auto" />
-            <Text>HomeScreen.</Text>
-            <Text>Não possui acesso?</Text>
-            <Button
-                title="Cadastre-se!"
-                onPress={() => navigation.navigate('Register')}/>
-            
-        </View>
-    )
+
+export default function HomeScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Card.Title
+        title="Advogados Cadastrados"
+      />
+      <Card>
+        <Card.Content>
+          <Title>Advogado: Joao Cesar</Title>
+          <Paragraph>OAB: 00000-0</Paragraph>
+        </Card.Content>
+      </Card>
+    </View>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+})
