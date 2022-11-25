82ba536aa54ede8a24fe3d3cb689bea7d1994bef
Luan Santos <luan@tootz.com.br>
2022-11-25 12:47:25 -0300

    feat: set authorization header to request when it exists

--- src/services/apiSuits.js
+++ src/services/apiSuits.js
@@ -1,10 +1,29 @@
-import axios from 'axios';
-
-const token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJyYWltYXIiLCJleHAiOjE2NjkzNDAyNzZ9.YxgE3vElB_JyHe2UoEEsisRRZSKpINUYGd4o31kDSGc";
+import axios from "axios";
 
 const apiSuits = axios.create({
-    baseURL: 'http://192.168.1.7:8080/api',
-    headers: {'Authorization': 'Bearer '+token}
+  baseURL: "http://192.168.1.7:8080/api",
 });
+
+apiSuits.interceptors.request.use(
+  (config) => {
+    // TODO: get token from async storage
+    config.headers[
+      "Authorization"
+    ] = `Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJyYWltYXIiLCJleHAiOjE2NjkzNDAyNzZ9.YxgE3vElB_JyHe2UoEEsisRRZSKpINUYGd4o31kDSGc`;
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+apiSuits.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default apiSuits;
